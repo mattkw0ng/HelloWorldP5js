a0f7216d4b30ac8ef1242cb9c662cbdc87eb268e
Matt <makwong@dons.usfca.edu>
2022-01-27 15:24:21 -0800

    Updated

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 // Setup code goes here
 function setup() {
   createCanvas(1400, 800);
-  // background(2);
+  textSize(24);
  }
 
 
@@ -10,7 +10,6 @@ function setup() {
  var clr = 0;
 // Draw code goes here
 function draw() {
-  fill(0);
   text("Hello World", width/2, height/2);
   if (mouseIsPressed) {
     fill(clr);
@@ -19,16 +18,14 @@ function draw() {
 }
 
 function keyPressed() {
-  console.log("keypressed");
-  if (keyCode === LEFT_ARROW) {
-    clr = clr - 1;
-  } else if (keyCode === RIGHT_ARROW) {
-    clr = clr + 1;
+  if (keyCode === ENTER) {
+    redraw();
   } else if (keyCode === UP_ARROW) {
     size = size + 1;
   } else if (keyCode === DOWN_ARROW) {
     size = size - 1;
   }
+  console.log("color: " + clr + "\nsize: " + size);
 }
 
 
